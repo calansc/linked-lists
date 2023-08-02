3499248dc37df0ca3753171ddb0e1e744ecf1051
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2023-08-02 10:57:05 -0400

    Initial commit. Add Node class, LinkedList class, append method

---
+++ script.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  //functions for linked list class here
+  append(value) {
+    let node = new Node(value);
+    let current;
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+  prepend(value) {
+    let node = new Node(value);
+  }
+}
